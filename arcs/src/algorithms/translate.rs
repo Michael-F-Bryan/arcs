28c8259391e27803d2b918e1ae796bd629c865ce
Michael Bryan <michaelfbryan@gmail.com>
2020-01-08 20:06:29 +0800

    Docs

--- arcs/src/algorithms/translate.rs
+++ arcs/src/algorithms/translate.rs
@@ -3,7 +3,7 @@ use crate::{
     Vector,
 };
 
-/// Something which can be moved around in *Drawing Space*.
+/// Something which can be moved around "rigidly" in *Drawing Space*.
 pub trait Translate {
     fn translate(&mut self, displacement: Vector);
 
