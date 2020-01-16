9f8d9671a6eb86ddaf1717c644a4a931ae3588cc
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-01-16 12:25:42 +0000

    Deploy Michael-F-Bryan/arcs to github.com/Michael-F-Bryan/arcs.git:gh-pages

--- implementors/std/io/trait.Read.js
+++ implementors/std/io/trait.Read.js
@@ -1,5 +1,6 @@
 (function() {var implementors = {};
 implementors["either"] = [{"text":"impl&lt;L, R&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/std/io/trait.Read.html\" title=\"trait std::io::Read\">Read</a> for <a class=\"enum\" href=\"either/enum.Either.html\" title=\"enum either::Either\">Either</a>&lt;L, R&gt; <span class=\"where fmt-newline\">where<br>&nbsp;&nbsp;&nbsp;&nbsp;L: <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/std/io/trait.Read.html\" title=\"trait std::io::Read\">Read</a>,<br>&nbsp;&nbsp;&nbsp;&nbsp;R: <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/std/io/trait.Read.html\" title=\"trait std::io::Read\">Read</a>,&nbsp;</span>","synthetic":false,"types":["either::Either"]}];
+implementors["rand_core"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/std/io/trait.Read.html\" title=\"trait std::io::Read\">Read</a> for dyn <a class=\"trait\" href=\"rand_core/trait.RngCore.html\" title=\"trait rand_core::RngCore\">RngCore</a>","synthetic":false,"types":["rand_core::RngCore"]}];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
--- rand/rngs/adapter/sidebar-items.js
+++ rand/rngs/adapter/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"struct":[["ReseedingRng","A wrapper around any PRNG that implements [`BlockRngCore`], that adds the ability to reseed it."]]});
+initSidebarItems({"struct":[["ReadRng","An RNG that reads random bytes straight from any type supporting [`std::io::Read`], for example files."],["ReseedingRng","A wrapper around any PRNG that implements [`BlockRngCore`], that adds the ability to reseed it."]]});
