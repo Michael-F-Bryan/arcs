2c0057f2456fd8b68fa7a07de991e8e4cffaa261
Lando <lando.schumpich@gmail.com>
2020-01-16 17:07:05 +0100

    Implements non uniform scaling via trait for Vector with test

--- arcs/src/algorithms/mod.rs
+++ arcs/src/algorithms/mod.rs
@@ -6,6 +6,7 @@ mod closest_point;
 mod length;
 mod translate;
 mod scale;
+mod scale_non_uniform;
 
 pub use approximate::{Approximate, ApproximatedArc};
 pub use bounding_box::Bounded;
@@ -13,3 +14,4 @@ pub use closest_point::{Closest, ClosestPoint};
 pub use length::Length;
 pub use translate::Translate;
 pub use scale::Scale;
+pub use scale_non_uniform::ScaleNonUniform;
---
+++ arcs/src/algorithms/scale_non_uniform.rs
@@ -0,0 +1,67 @@
+use crate::{
+    Vector,
+};
+use kurbo::Affine;
+
+/// Something which can be scaled **non-uniform** in x and y directions in *Drawing Space*
+pub trait ScaleNonUniform {
+    fn scale_nu(&mut self, factor_x: f64, factor_y: f64, base: Vector);
+
+    fn scaled_nu(&self, factor_x: f64, factor_y: f64, base: Vector) -> Self
+    where 
+        Self: Sized + Clone,
+        {
+            let mut clone = self.clone();
+            clone.scale_nu(factor_x, factor_y, base);
+
+            clone
+        }
+
+}
+
+impl<'t, T: ScaleNonUniform + ?Sized> ScaleNonUniform for &'t mut T {
+    fn scale_nu(&mut self, factor_x: f64, factor_y: f64, base: Vector) {
+        (*self).scale_nu(factor_x, factor_y, base);
+    }
+}
+
+impl ScaleNonUniform for Vector {
+    fn scale_nu(&mut self, factor_x: f64, factor_y: f64, base: Vector) {
+        let translate_to_base = Affine::translate(base * -1.0);
+        let scale = kurbo_scale_nu(factor_x, factor_y);
+        let translate_back = Affine::translate(base);
+        let combined_transform = translate_back * scale * translate_to_base;
+        let new_pos = combined_transform * *self;
+        self.x = new_pos.x;
+        self.y = new_pos.y;
+    }
+}
+
+#[inline]
+const fn kurbo_scale_nu(factor_x: f64, factor_y: f64) -> Affine {
+    Affine::new([factor_x, 0.0, 0.0, factor_y, 0.0, 0.0])
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn vector() {
+        let original = Vector::new(-1.0, 5.0);
+        let factor_x = 2.0;
+        let factor_y = 2.5;
+
+        let actual = original.scaled_nu(factor_x, factor_y, Vector::zero());
+        // known value
+        let expected = Vector::new(-2.0, 12.5);
+
+        assert_eq!(actual, expected);
+
+        let base = Vector::new(2.0, 0.0);
+        let actual = original.scaled_nu(factor_x, factor_y, base);
+        let expected = Vector::new(-4.0, 12.5);
+
+        assert_eq!(actual, expected);
+    }
+}
