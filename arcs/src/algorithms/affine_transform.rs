675f23f4c2708640d4a7c8d262bfa39032b2bd84
Lando <lando.schumpich@gmail.com>
2020-01-23 17:49:14 +0100

    Removes ase requirement for AffineTransformable

--- arcs/src/algorithms/affine_transform.rs
+++ arcs/src/algorithms/affine_transform.rs
@@ -5,46 +5,43 @@ use crate::{
 use kurbo::Affine;
 
 pub trait AffineTransformable {
-    fn transform(&mut self, affine: Affine, base: Vector);
+    fn transform(&mut self, affine: Affine);
 
-    fn transformed(&self, affine: Affine, base: Vector) -> Self
+    fn transformed(&self, affine: Affine) -> Self
     where 
         Self: Sized + Clone,
         {
             let mut clone = self.clone();
-            clone.transform(affine, base);
+            clone.transform(affine);
 
             clone
         }
 }
 
 impl <'t, T: AffineTransformable + ?Sized> AffineTransformable for &'t mut T {
-    fn transform(&mut self, affine: Affine, base: Vector) {
-        (*self).transform(affine, base);
+    fn transform(&mut self, affine: Affine) {
+        (*self).transform(affine);
     }
 }
 
 impl AffineTransformable for Vector {
-    fn transform(&mut self, affine: Affine, base: Vector) {
-        let translate_to_base = Affine::translate(base * -1.0);
-        let translate_back = Affine::translate(base);
-        let combined_transform = translate_back * affine * translate_to_base;
-        let new_pos = combined_transform * *self;
+    fn transform(&mut self, affine: Affine) {
+        let new_pos = affine * *self;
         self.x = new_pos.x;
         self.y = new_pos.y;
     }
 }
 
 impl AffineTransformable for Point {
-    fn transform(&mut self, affine: Affine, base: Vector) {
-        self.location.transform(affine, base);
+    fn transform(&mut self, affine: Affine) {
+        self.location.transform(affine);
     }
 }
 
 impl AffineTransformable for Line {
-    fn transform(&mut self, affine: Affine, base: Vector) {
-        self.start.transform(affine, base);
-        self.end.transform(affine, base);
+    fn transform(&mut self, affine: Affine) {
+        self.start.transform(affine);
+        self.end.transform(affine);
     }
 }
 
--- arcs/src/algorithms/scale_non_uniform.rs
+++ arcs/src/algorithms/scale_non_uniform.rs
@@ -1,19 +1,18 @@
 use crate::{
-    Vector,
     algorithms::{AffineTransformable},
 };
 use kurbo::Affine;
 
 /// Something which can be scaled **non-uniform** in x and y directions in *Drawing Space*
 pub trait ScaleNonUniform {
-    fn scale_non_uniform(&mut self, factor_x: f64, factor_y: f64, base: Vector);
+    fn scale_non_uniform(&mut self, factor_x: f64, factor_y: f64);
 
-    fn scaled_non_uniform(&self, factor_x: f64, factor_y: f64, base: Vector) -> Self
+    fn scaled_non_uniform(&self, factor_x: f64, factor_y: f64) -> Self
     where 
         Self: Sized + Clone,
         {
             let mut clone = self.clone();
-            clone.scale_non_uniform(factor_x, factor_y, base);
+            clone.scale_non_uniform(factor_x, factor_y);
 
             clone
         }
@@ -21,9 +20,9 @@ pub trait ScaleNonUniform {
 }
 
 impl<A: AffineTransformable> ScaleNonUniform for A {
-    fn scale_non_uniform(&mut self, factor_x: f64, factor_y: f64, base: Vector){
+    fn scale_non_uniform(&mut self, factor_x: f64, factor_y: f64){
         // TODO: Change to `Affine::scale_non_uniform()` after crates.io update
-        self.transform(Affine::new([factor_x, 0.0, 0.0, factor_y, 0.0, 0.0]), base);
+        self.transform(Affine::new([factor_x, 0.0, 0.0, factor_y, 0.0, 0.0]));
     }
 }
 
@@ -31,6 +30,8 @@ impl<A: AffineTransformable> ScaleNonUniform for A {
 mod tests {
     use super::*;
     use crate::primitives::{Line};
+    use crate::algorithms::Translate;
+    use crate::Vector;
 
     #[test]
     fn scale_vector() {
@@ -40,7 +41,7 @@ mod tests {
         let factor_x = 2.0;
         let factor_y = 2.5;
 
-        let actual = original.scaled_non_uniform(factor_x, factor_y, Vector::zero());
+        let actual = original.scaled_non_uniform(factor_x, factor_y);
         // known value
         let expected = Vector::new(x * factor_x, y * factor_y);
 
@@ -52,12 +53,27 @@ mod tests {
         let original = Vector::new(-1.0, 5.0);
         let factor_x = 2.0;
         let factor_y = 2.5;
-
         let base = Vector::new(2.0, 0.0);
-        let actual = original.scaled_non_uniform(factor_x, factor_y, base);
-        let expected = Vector::new(-4.0, 12.5);
 
-        assert_eq!(actual, expected);
+        let expected = Vector::new(-4.0, 12.5);
+        
+        // We can either use explicit transformation methods:
+        let mut transformed = original.translated(Vector::zero() - base);
+        transformed.scale_non_uniform(factor_x, factor_y);
+        transformed.translate(base);
+
+        assert_eq!(transformed, expected);
+
+        // Or compose an `Affine` and pass it directly to the `transform` method:
+        // keep in mind that transforms get composed *in reverse execution order*
+        let translate_to_origin = Affine::translate(Vector::zero() - base);
+        let scale_non_uniform = Affine::new([factor_x, 0.0, 0.0, factor_y, 0.0, 0.0]);
+        let translate_back = Affine::translate(base);
+        let combined_transform = translate_back * scale_non_uniform * translate_to_origin;
+        
+        let transformed = original.transformed(combined_transform);
+
+        assert_eq!(transformed, expected);
     }
 
     #[test]
@@ -68,7 +84,7 @@ mod tests {
         let factor_x = 1.5;
         let factor_y = -2.0;
 
-        let actual = original.scaled_non_uniform(factor_x, factor_y, Vector::zero());
+        let actual = original.scaled_non_uniform(factor_x, factor_y);
         let expected = Line::new(Vector::new(2.0 * factor_x, 4.0 * factor_y), Vector::new(3.0 * factor_x, -5.0 * factor_y));
 
         assert_eq!(actual, expected);
@@ -81,12 +97,15 @@ mod tests {
         let original = Line::new(start, end);
         let factor_x = 1.5;
         let factor_y = -2.0;
+
+        let expected = Line::new(Vector::new(1.75, -9.5), Vector::new(3.25, 8.5));
         
         // scale by line mid-point as reference
-        let actual = original.scaled_non_uniform(factor_x, factor_y, start + original.displacement() * 0.5);
-        let expected = Line::new(Vector::new(1.75, -9.5), Vector::new(3.25, 8.5));
+        let mid_point = start + original.displacement() * 0.5;
+        let mut transformed = original.translated(Vector::zero() - mid_point);
+        transformed.scale_non_uniform(factor_x, factor_y);
+        transformed.translate(mid_point);
 
-        assert_eq!(actual, expected);
-        
+        assert_eq!(transformed, expected);
     }
 }
