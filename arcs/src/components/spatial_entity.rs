cae53ff1353a1b86e3b5e2feb6e06d6d8aa0796a
Lando <lando.schumpich@gmail.com>
2020-02-01 12:29:30 +0100

    Removes test from *Space* As we do the testing inside of the *SpatialRelation* system now

--- arcs/src/components/spatial_entity.rs
+++ arcs/src/components/spatial_entity.rs
@@ -119,7 +119,7 @@ impl Space {
     }
 
     pub fn query_point(&self, point: Vector) -> Option<Vec<Entity>> {
-        let cursor_circle = Arc::from_centre_radius(point, 1.0, 0.0, 2.0 * std::f64::consts::PI);
+        let cursor_circle = Arc::from_centre_radius(point, Self::QUERY_POINT_RADIUS, 0.0, 2.0 * std::f64::consts::PI);
         let query = self.quadtree.query(cursor_circle.bounding_box().aabb());
         
         if query.is_empty() {
@@ -138,100 +138,5 @@ impl Space {
     pub fn clear(&mut self) {
         self.quadtree = Self::default_tree();
         self.ids.clear();
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use crate::{
-        primitives::{Line, Arc},
-        components::{BoundingBox, SpatialEntity, register, Layer, Name, DrawingObject, Geometry, LineStyle, Dimension},
-        Vector,
-        algorithms::{Bounded},
-    };
-    use aabb_quadtree::{QuadTree, Spatial, ItemId};
-    use euclid::{TypedRect};
-    use std::f64::consts::PI;
-    use specs::prelude::*;
-    use piet::Color;
-
-    #[test]
-    fn line_insertion() {
-        let mut world = World::new();
-
-        // make sure we register all components
-        register(&mut world);
-    
-        let layer = Layer::create(
-            world.create_entity(),
-            Name::new("default"),
-            Layer {
-                z_level: 0,
-                visible: true,
-            },
-        );
-    
-        // Add a line to our world
-        let line = Line::new(Vector::new(2.0, 1.0), Vector::new(5.0, -1.0));
-        world
-            .create_entity()
-            .with(DrawingObject {
-                geometry: Geometry::Line(line),
-                layer,
-            })
-            .with(LineStyle {
-                width: Dimension::DrawingUnits(5.0),
-                stroke: Color::rgb8(0xff, 0, 0),
-            })
-            .build()
-        ;
-
-        // Create the Quadtree to hold all spacial entities
-        // Size needs to be bigger then everything we plan on drawing :/
-        let max_size = 1_000_000.0;
-        let size = BoundingBox::new(Vector::new(-max_size, -max_size), Vector::new(max_size, max_size)).aabb();
-        // I don't really understand why we need to specify A -> [(ItemId, TypedRect<f32, u64>); 0]
-        let mut qt: QuadTree<SpatialEntity, f64, [(ItemId, TypedRect<f32, f64>); 0]> = QuadTree::new(
-            size,
-            true,
-            4,
-            16,
-            8,
-            4
-        );        
-
-        // get the lines entity from the world
-        let mut id: Option<ItemId> = None;
-        let drawing_storage = world.read_storage::<DrawingObject>();
-        for entity in world.entities().join() {
-            if let Some(drawing) = drawing_storage.get(entity) {
-                match drawing.geometry {
-                    Geometry::Line(l) => {
-                        // Add to quad tree
-                        id = qt.insert(SpatialEntity::new(l.bounding_box(), entity));
-                        assert_eq!(qt.get(id.unwrap()).unwrap().entity, entity);
-                    },
-                    _ => (),
-                }
-            }
-        }
-        
-        // simulate cursor query at line start
-        let cursor_pos = Vector::new(2.0, 1.0);
-        let cursor_circle = Arc::from_centre_radius(cursor_pos, 1.0, 0.0, 2.0 * PI);
-
-        let query = qt.query(cursor_circle.bounding_box().aabb());
-        assert_eq!(query.len(), 1);
-        assert_eq!(query[0].2, id.unwrap());
-
-        // simulate cursor query at line end
-        // FIXME: Fails if we input exactly line end point
-        let cursor_pos = Vector::new(4.9, -0.9);
-        let cursor_circle = Arc::from_centre_radius(cursor_pos, 1.0, 0.0, 2.0 * PI);
-
-        let query = qt.query(cursor_circle.bounding_box().aabb());
-        assert_eq!(query.len(), 1);
-        assert_eq!(query[0].2, id.unwrap());
-
     }
 }
